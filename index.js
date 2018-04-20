fee59f3d3021c78506b5b913c5be3117d3eeb51c
Chrissy Hunt <chrissy.hunt@gmail.com>
2018-04-20 21:46:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function nameToAttributes(array) {
 }
 
 function attributesToPhrase(array) {
+  console.log(array)
   for (const obj in array) {
     console.log(`${obj['name']} is from ${obj['hometown']}`)
     return `${obj['name']} is from ${obj['hometown']}`; 
