d289b386f61dd5d576b2dfec944531e76345b6e3
Chrissy Hunt <chrissy.hunt@gmail.com>
2018-04-20 21:43:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function lowerCaseDrivers(array) {
 function nameToAttributes(array) {
   const splitArr = array.map(function(fullName) {
     const splitName = fullName.split(" ");
-    console.log(splitName);
     return Object.assign({}, { firstName: splitName[0], lastName: splitName[1]});
   })
   
   return splitArr;
 }
+
