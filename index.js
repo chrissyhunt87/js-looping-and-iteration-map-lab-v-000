817e39f8ccd55a9d5cf4acb16f3647578a0a508b
Chrissy Hunt <chrissy.hunt@gmail.com>
2018-04-20 21:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(array) {
+  const newArr = array.map(function(name) { 
+    return name.toLowerCase(); 
+  });
+  
+  return newArr;
+}
+
+function nameToAttributes(array) {
+  const splitArr = array.map(function(fullName) {
+    const splitName = fullName.split(" ");
+    console.log(splitName);
+    return Object.assign({}, { firstName: splitName[0], lastName: splitName[1]});
+  })
+  
+  debugger
+  console.log(split.Arr);
+  return splitArr;
+}
